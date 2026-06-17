8d758b01baa858f80f040baf19d9d718eb384781
Nguyen Dieu Vi <nguyendieuvi1230@gmail.com>
2026-06-17 22:39:00 +0700

    delete unnescessary

--- lib/models/publication.dart
+++ lib/models/publication.dart
@@ -9,10 +9,6 @@ class Publication {
   final String? doi;
   final String? abstractText;
   final List<String> authors;
-  final List<String> concepts;
-  final List<String> institutions;
-  final List<String> countries;
-  final Map<int, int> citationsByYear;
 
   Publication({
     required this.id,
@@ -23,10 +19,6 @@ class Publication {
     required this.doi,
     required this.abstractText,
     required this.authors,
-    this.concepts = const [],
-    this.institutions = const [],
-    this.countries = const [],
-    this.citationsByYear = const {},
   });
 
   factory Publication.fromJson(Map<String, dynamic> json) {
@@ -44,28 +36,6 @@ class Publication {
           .map((item) => item['author']?['display_name']?.toString())
           .whereType<String>()
           .toList(),
-      concepts: (json['concepts'] as List? ?? [])
-          .where((c) => (c['score'] as num? ?? 0) > 0.3)
-          .map((c) => c['display_name']?.toString() ?? '')
-          .where((s) => s.isNotEmpty)
-          .toList(),
-      institutions: (json['authorships'] as List? ?? [])
-          .expand((a) => (a['institutions'] as List? ?? []))
-          .map((i) => i['display_name']?.toString() ?? '')
-          .where((s) => s.isNotEmpty)
-          .toList(),
-      countries: (json['authorships'] as List? ?? [])
-          .expand<String>((a) => (a['countries'] as List? ?? []).cast<String>())
-          .where((s) => s.isNotEmpty)
-          .toList(),
-      citationsByYear: Map.fromEntries(
-        (json['counts_by_year'] as List? ?? []).map(
-          (e) => MapEntry(
-            e['year'] as int,
-            e['cited_by_count'] as int? ?? 0,
-          ),
-        ),
-      ),
     );
   }
 
